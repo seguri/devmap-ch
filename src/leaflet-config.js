bee2c2273adbcdabbcf257285981085dad660d6f
Marco Seguri <seguri@users.noreply.github.com>
2025-02-09 00:37:24 +0100

    Customize pin icon

--- src/leaflet-config.js
+++ src/leaflet-config.js
@@ -1,6 +1,6 @@
 import L from "leaflet";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "./images/marker-icon.png";
+import markerIcon2x from "./images/marker-icon-2x.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 L.Icon.Default.mergeOptions({
