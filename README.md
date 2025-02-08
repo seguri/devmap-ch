3f1757d3a9dbe8dcfdf8cdd641fd27a53afb684b
Marco Seguri <seguri@users.noreply.github.com>
2025-02-08 01:29:36 +0100

    Focus on Switzerland

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # devmap.ch
+
+## SHP to GeoJSON
+
+- Download the SHP from [swisstopo](https://www.swisstopo.admin.ch/en/landscape-model-swissboundaries3d)
+- Extract the country borders: `*_LANDESGEBIET.shp`, `*_LANDESGEBIET.dbf`, `*_LANDESGEBIET.shx`
+- Convert to GeoJSON on mygeodata.cloud (coordinate system: `LV95 LN02`)
+- Download and rename to `ch-3d.geojson`
+- Delete all height information (the third coordinate in each coordinate array) with:
+
+    ```
+    jq 'walk(if type == "array" and length == 3 and all(type == "number") then [.[0], .[1]] else . end)' ch-3d.geojson > ch-2d.geojson
+    ```
+
+- Manually remove Germany, Liechtenstein and Italia from `ch-2d.geojson`
+- Halve the number of coordinates of each ring with:
+
+    ```
+    jq '.features[0].geometry.coordinates = [.features[0].geometry.coordinates[] | [.[range(0; length; 2)]]]' ch-2d.geojson > ch-2d-half.geojson
+    ```
+
+- Minify with:
+
+    ```
+    jq -c . ch-2d-half.geojson > ch-2d-half.min.geojson
+    ```
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import 'leaflet/dist/leaflet.css';
-import './styles.css'
-import L from 'leaflet';
+import "leaflet/dist/leaflet.css";
+import "./styles.css";
+import L from "leaflet";
+import swissBorderText from "bundle-text:../data/ch-2d-half.min.geojson";
 
 const map = createMap();
 addAttribution(map);
+addSwissBorder(map);
 
 function createMap() {
-    return L.map('map').fitWorld();
+  return L.map("map").setView([46.8, 8.2], 8);
 }
 
 function addAttribution(map) {
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>
+      | <a href="https://www.swisstopo.admin.ch/en/geodata/landscape/boundaries3d.html">swisstopo</a>`,
+  }).addTo(map);
+}
+
+function addSwissBorder(map) {
+  const swissBorderData = JSON.parse(swissBorderText);
+  const pantone485c = "#D20001";
+  L.geoJSON(swissBorderData, {
+    style: {
+      color: pantone485c,
+    },
+  }).addTo(map);
 }
